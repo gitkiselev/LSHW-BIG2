42c7f98910edf7ba5454a8772da4cba485a47762
gitkiselev <482003@mail.ru>
2016-05-06 13:04:10 +0300

    First upload

---
+++ app/js/script.js
@@ -0,0 +1,234 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap;
+    var reviewFormAddress = document.querySelector('.header__title');//Адрес места отзыва
+    var addReview         = document.querySelector('.feedback__link');
+    var areaUser          = document.querySelector('.feedback-area__user');
+
+    var areaReview        = document.querySelector('.feedback-area__review');
+
+    var btnClose          = document.querySelector('.header__close-btn');//Закрыть попап окно
+    //Переменные формы
+    var userName          = document.querySelector('.feedback__name');
+    var userPlace         = document.querySelector('.feedback__place');
+    var userMessage       = document.querySelector('.feedback__text');
+
+    var addReview         = document.querySelector('.feedback__link'); //Добавить  
+
+    var reviewForm        = document.querySelector('.app');//Окно
+
+    
+
+//coords
+    var coords;
+    var coordX;
+    var coordY;
+//objects
+    var places = [];
+
+    myMap = new ymaps.Map("map", {
+        center: [55.755381, 37.619044],
+        zoom: 11
+
+
+    });
+//Кластеризатор
+    var clusterer = new ymaps.Clusterer({
+        preset: 'islands#invertedVioletClusterIcons',
+        groupByCoordinates: false,
+        clusterDisableClickZoom: true,
+        clusterHideIconOnBalloonOpen: false,
+        geoObjectHideIconOnBalloonOpen: false,
+        clusterBalloonContentLayout: 'cluster#balloonCarousel',
+        clusterBalloonPagerSize: 5
+    });
+    //Отрисовка всех меток
+    renderMarks();
+
+    //show popup on map click
+    myMap.events.add('click', popupShow);
+
+    function popupShow(e) {
+        //show popup onclick
+        var coord = e.get('coords');
+        document.querySelector('.feedback-area__list').innerHTML = "Отзывов пока нет";
+        coords = coord;
+        coordX = coords[0];
+        coordY = coords[1];
+        var pagePixels = e.get('pagePixels');
+        reviewForm.style.left = pagePixels[0] + 'px';
+        reviewForm.style.top = pagePixels[1] + 'px';
+        reviewForm.classList.remove('display-none');
+        ymaps.geocode(coords).then(function (res) {
+            var firstGeoObject = res.geoObjects.get(0);
+            var address = firstGeoObject.properties.get('name') + ',' + ' ' + firstGeoObject.properties.get('description').split(', ')[1];
+            //set address in popup head
+            reviewFormAddress.innerText = address;
+        });
+    }
+
+    //get marks from server & render
+    function renderMarks() {
+        var xhr = new XMLHttpRequest();
+        //запрос серверу какой именно кейс вернуть
+        var reviewData = {'op': "all"};
+        xhr.open('POST', 'http://smelukov.com:3000', true);
+        xhr.send(JSON.stringify(reviewData));
+        //событие "запрос завершен"
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) return;
+            //получаем объект
+            reviewsBase = JSON.parse(xhr.response);
+            //перебираем ключи массива объектов
+            for (var keys in reviewsBase) {
+                //получаем значения координат каждого ключа coords
+                reviewsBase[keys].forEach(function (key) {
+                    var X = key.coords.x;
+                    var Y = key.coords.y;
+                    //указываем, какие данные выводить для точки
+                    var place = new ymaps.Placemark([X, Y], {
+                        balloonContentHeader: key.name,
+                        balloonContentBody: key.text,
+                        balloonContentFooter: new Date(key.date).toLocaleDateString(),
+                        // Задаем стиль метки (метка в виде круга).
+                        preset: "islands#dotCircleIcon",
+                        // Задаем цвет метки (в формате RGB).
+                        iconColor: '#ff0000'
+                    });
+                    //добавляем готовую к рендеру точку в массив мест с отзывами
+                    places.push(place);
+                })
+            }
+            //добавляем массив точек к api кластеризации
+            clusterer.add(places);
+            //выводим кластеризацию на карту
+            myMap.geoObjects.add(clusterer);
+        };
+
+
+    }
+
+    //send data to server with ajax
+    addReview.addEventListener('click', sendDataAjax);
+    function sendDataAjax(e) {
+        console.log('Добавить');
+        e.preventDefault();
+        var mark = new ymaps.Placemark([coordX, coordY], {}, {
+            // Задаем стиль метки (метка в виде круга).
+            preset: "islands#dotCircleIcon",
+            // Задаем цвет метки (в формате RGB).
+            iconColor: '#ff0000'
+        });
+        var address = reviewFormAddress.innerText;
+        var name    = userName.value;
+        var place   = userPlace.value;
+        var text    = userMessage.value;
+        var date    = new Date();
+
+        //data obj to send
+        var userData = {
+            "op": "add",
+            "review": {
+                "coords": {'x': coordX, 'y': coordY},
+                "address": address,
+                "name": name,
+                "place": place,
+                "text": text,
+                "date": date.toUTCString()
+            }
+        };
+
+
+        //ajax send data
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://smelukov.com:3000', true);
+        xhr.send(JSON.stringify(userData));
+        xhr.onreadystatechange = function () {  
+            if (xhr.readyState === 4) {
+                console.log("complete");
+
+                renderReviews(address);
+
+                //ставим метку только по OK от сервера
+                myMap.geoObjects.add(mark);
+                places.push(userData.review);
+                //кластеризация добавленных точек
+                clusterer.add(mark);
+
+                myMap.geoObjects.add(clusterer);
+            } else {
+                return;
+            }
+        };
+        userName.value    = '';
+        userPlace.value   = '';
+        userMessage.value = '';
+
+
+    }
+
+    function renderReviews(address) {
+
+        var getDataRequest = {
+            "op": "get",
+            "address": address
+        };
+
+        //ajax send data
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://smelukov.com:3000', true);
+        xhr.send(JSON.stringify(getDataRequest));
+        xhr.onreadystatechange = function () {  // Формируем функцию срабатывания на успешный ответ от сервера
+            if (xhr.readyState === 4) {
+                //передаем данные отзывов в попап
+                var content = document.querySelector('.feedback-area__list');
+                var source = reviewTemplate.innerHTML;
+                var templateFn = Handlebars.compile(source);
+                var response = xhr.responseText;
+                var responseObj = JSON.parse(response);
+
+                for (var i=0; i<responseObj.length; i++){
+                    var dateString = new Date(responseObj[i].date).toLocaleDateString();
+                    responseObj[i].date = dateString;
+                }
+
+                var viewsTemplate = templateFn({
+                    data: responseObj
+
+                });
+
+                content.innerHTML = viewsTemplate;
+            } else {
+                return;
+            }
+        };
+
+    }
+
+    
+    //popup close listener
+    btnClose.addEventListener('click', closePopup);
+    function closePopup() {
+        reviewForm.classList.add('display-none');
+        reviewFormAddress.innerText = '';
+        userName.value = '';
+        userPlace.value = '';
+        userMessage.value = '';
+    }
+
+//end init func
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+Установка проекта. 
+
+1. Скачать node.js + npm c официального сайта https://nodejs.org/download/
+2. Установить git с официального сайта http://git-scm.com/downloads
+3. Установить gulp глобально npm install --global gulp-cli
+В консоли войти в папку с проектом
+
+3. Установить gulp локально npm install --save-dev gulp
+4. Зайти на сайт https://github.com/
+5. Перейти на страницу проекта https://github.com/gitkiselev/
+6. Нажать кнопку download.zip
